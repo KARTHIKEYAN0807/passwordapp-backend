e7b82a0d64d28ed5f727c996bfb2a66ee6158034
KARTHIKEYAN0807 <karthickestore.37sp@gmail.com>
2024-09-09 17:58:19 +0530

    b

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Ensure this matches the frontend URL
+  origin: 'https://passwordapp-frontend.netlify.app/', // Ensure this matches the frontend URL
   credentials: true,
 }));
 app.use(express.json());
