1ebc1761d6ec70916623678a98b6fcab42896b8f
KARTHIKEYAN0807 <karthickestore.37sp@gmail.com>
2024-09-09 16:41:41 +0530

    backend

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://password-reset-frontend-client.netlify.app/', // Ensure this matches the frontend URL
-  credentials: true,
+  origin: 'https://password-reset-frontend-client.netlify.app', // Ensure this matches the frontend URL
+  // credentials: true,
 }));
-app.options('*', cors({   origin: 'https://password-reset-frontend-client.netlify.app',   credentials: true, }));
+// app.options('*', cors({   origin: 'https://password-reset-frontend-client.netlify.app',   credentials: true, }));
 app.use(express.json());
 app.use(morgan('dev')); // Log requests to the console
 
